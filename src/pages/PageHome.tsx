39b9775fe8d5cdb1b09c010f5035ab9a028b0dee
nemesnyik.zsolt <zsolt.nemesnyik@webery.com>
2026-01-18 10:16:50 +0100

    add React Link component

--- src/pages/PageHome.tsx
+++ src/pages/PageHome.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 export default function PageHome() {
   return (
@@ -6,7 +7,7 @@ export default function PageHome() {
       <section className="flex gap-y-10 gap-x-5 h-full flex-wrap justify-center p-24">
         <h1 className="text-3xl font-bold w-full">Hello, this is a JS based dice game app! Feel free to play!</h1>
         <Button className="mt-4" variant="default" asChild>
-          <a href="/game">Play</a>
+          <Link to="/game">Play</Link>
         </Button>
       </section>
     </main>
