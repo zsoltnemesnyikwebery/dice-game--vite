4f7705025ca4179bb2ac3d93f1ba4174de94132c
nemesnyik.zsolt <zsolt.nemesnyik@webery.com>
2026-01-20 13:02:29 +0100

    update PageHome component: remove <main> tag

--- src/pages/PageHome.tsx
+++ src/pages/PageHome.tsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 export default function PageHome() {
   return (
-    <main className="container mx-auto px-5 flex min-h-screen flex-col items-center justify-center">
-      <section className="flex gap-y-10 gap-x-5 h-full flex-wrap justify-center p-24">
-        <h1 className="text-3xl font-bold w-full">Hello, this is a JS based dice game app! Feel free to play!</h1>
-        <Button className="mt-4" variant="default" asChild>
-          <Link to="/game">Play</Link>
-        </Button>
-      </section>
-    </main>
-  )
+    <section className="flex gap-y-10 gap-x-5 h-full flex-wrap justify-center p-24">
+      <h1 className="text-3xl font-bold w-full">
+        Hello, this is a JS based dice game app! Feel free to play!
+      </h1>
+      <Button className="mt-4" variant="default" asChild>
+        <Link to="/game">Play</Link>
+      </Button>
+    </section>
+  );
 }
