9fc884df028a461fcd7be4022431836e2a67d256
nemesnyik.zsolt <zsolt.nemesnyik@webery.com>
2026-02-06 12:44:37 +0100

    restructure - update Player type: remove actions, add roll

--- src/models/types.ts
+++ src/models/types.ts
@@ -2,6 +2,7 @@ import type { GAME_STATUS } from "@/lib/constants";
 
 export type DiceValue = 1 | 2 | 3 | 4 | 5 | 6;
 export type DiceRoll = DiceValue[];
+export type RollType = "normal" | "celebration" | "destroy"
 export type GameStatus = typeof GAME_STATUS[keyof typeof GAME_STATUS];
 export type Player = {
   id: string;
@@ -9,13 +10,13 @@ export type Player = {
   avatar: string;
   color: string;
   isActive: boolean;
-  rolls: DiceRoll | undefined;
+  roll?: {
+    dice: DiceRoll;
+    score: number;
+    type: RollType;
+  };
   score: {
     current: number;
     total: number;
-  };
-  actions: {
-    celebration: boolean;
-    destroy: boolean;
   };
 };
